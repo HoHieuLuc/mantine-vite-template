7e34caebb4ce0ce4819fba0fca25b86da6b43b5e
Ho Hieu Luc <kingofika123@gmail.com>
2022-10-30 00:02:11 +0700

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import { ColorScheme, ColorSchemeProvider, MantineProvider } from '@mantine/core';
-import { useLocalStorage } from '@mantine/hooks';
 import { ColorSchemeToggle } from './components/ColorSchemeToggle/ColorSchemeToggle';
 import Welcome from './components/Welcome/Welcome';
+import AppProvider from './providers';
 
 function App() {
-    const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
-        key: 'mantine-color-scheme',
-        defaultValue: 'light',
-        getInitialValueInEffect: true,
-    });
-
-    const toggleColorScheme = (value: ColorScheme) => {
-        setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
-    };
 
     return (
-        <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
-            <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
-                <Welcome />
-                <ColorSchemeToggle />
-            </MantineProvider>
-        </ColorSchemeProvider>
+        <AppProvider>
+            <Welcome />
+            <ColorSchemeToggle />
+        </AppProvider>
     );
 }
 
