46535232fd94e500aa0f8ebf8af90b805e638adf
Ho Hieu Luc <kingofika123@gmail.com>
2022-10-30 00:04:26 +0700

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { ColorSchemeToggle } from './components/ColorSchemeToggle/ColorSchemeToggle';
 import Welcome from './components/Welcome/Welcome';
-import AppProvider from './providers';
 
 function App() {
 
     return (
-        <AppProvider>
+        <>
             <Welcome />
             <ColorSchemeToggle />
-        </AppProvider>
+        </>
     );
 }
 
